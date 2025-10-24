64e467bdd6bc9da02229f33e4be8fc2e67bc3a39
AvijitDasAvi <avijitavi338895@gmail.com>
2025-10-25 05:47:06 +0600

    Sign In Screen done.

--- lib/features/auth/sign_in/screen/sign_in_screen.dart
+++ lib/features/auth/sign_in/screen/sign_in_screen.dart
@@ -1,10 +1,198 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:yousef1234321/core/common/constants/app_colors.dart';
+import 'package:yousef1234321/core/common/constants/iconpath.dart';
+import 'package:yousef1234321/core/common/widgets/social_button.dart';
+import 'package:yousef1234321/features/auth/sign_in/controller/sign_in_controller.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white);
+    final controller = Get.put(SignInController());
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 47, right: 47),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Align(
+              //   alignment: Alignment.topLeft,
+              //   child: Container(
+              //     height: 44,
+              //     width: 44,
+              //     decoration: BoxDecoration(
+              //       color: Color(0xFFE8F1FD),
+              //       borderRadius: BorderRadius.circular(100),
+              //     ),
+              //     child: Center(
+              //       child: Icon(
+              //         Icons.arrow_back_ios,
+              //         color: AppColors.splashButtonColor,
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              SizedBox(height: 40),
+              Text(
+                'Sign In Now',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Please sign in to continue our app',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: AppColors.subTextColor,
+                ),
+              ),
+              SizedBox(height: 40),
+              TextField(
+                controller: controller.emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "Email address",
+                  prefixIcon: const Icon(Icons.email_outlined),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 18,
+                    horizontal: 20,
+                  ),
+                  filled: true,
+                  fillColor: Color(0xFFF7F7F9),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(14),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 24),
+              Obx(
+                () => TextField(
+                  controller: controller.passwordController,
+                  obscureText: !controller.isPasswordVisible.value,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    prefixIcon: const Icon(Icons.lock_outline),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        controller.isPasswordVisible.value
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: controller.togglePasswordVisibility,
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 18,
+                      horizontal: 20,
+                    ),
+                    filled: true,
+                    fillColor: Color(0xFFF7F7F9),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Align(
+                alignment: Alignment.centerRight,
+                child: GestureDetector(
+                  onTap: () {},
+                  child: const Text(
+                    "Forgot password?",
+                    style: TextStyle(
+                      color: AppColors.splashButtonColor,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 63),
+              SizedBox(
+                width: double.infinity,
+                height: 48,
+                child: ElevatedButton(
+                  onPressed: controller.isLoading.value
+                      ? null
+                      : controller.signIn,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.splashButtonColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                  ),
+                  child: controller.isLoading.value
+                      ? const CircularProgressIndicator(color: Colors.white)
+                      : const Text(
+                          "Sign In",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
+                        ),
+                ),
+              ),
+              SizedBox(height: 48),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  text: "Don't have an account? ",
+                  style: const TextStyle(
+                    color: AppColors.subTextColor,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: "  Sign Up",
+                      style: const TextStyle(
+                        color: AppColors.splashButtonColor,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Or",
+                style: TextStyle(
+                  color: AppColors.subTextColor,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(height: 15),
+
+              SocialButton(
+                imagePath: Iconpath.google,
+                text: 'Continue with Google',
+                onTap: () {},
+              ),
+              SizedBox(height: 24),
+              SocialButton(
+                imagePath: Iconpath.apple,
+                text: 'Continue with Apple',
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
