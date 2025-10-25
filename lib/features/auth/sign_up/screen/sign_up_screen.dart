ed96aa14183b214f00eeb0eac53f4b457fe427a0
AvijitDasAvi <avijitavi338895@gmail.com>
2025-10-25 06:24:57 +0600

    Sign Up Screen done.

---
+++ lib/features/auth/sign_up/screen/sign_up_screen.dart
@@ -0,0 +1,235 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:yousef1234321/core/common/constants/app_colors.dart';
+import 'package:yousef1234321/core/common/constants/iconpath.dart';
+import 'package:yousef1234321/core/common/widgets/social_button.dart';
+import 'package:yousef1234321/features/auth/sign_in/controller/sign_in_controller.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(SignInController());
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: GestureDetector(
+                  onTap: () {
+                    Get.back();
+                    if (kDebugMode) {
+                      print('Tapped');
+                    }
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 24, top: 24),
+                    height: 44,
+                    width: 44,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFE8F1FD),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Center(
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        color: AppColors.splashButtonColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 47, right: 47),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 40),
+                    Text(
+                      'Sign Up Now',
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'Please fill the details and create account',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: AppColors.subTextColor,
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    TextField(
+                      controller: controller.emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        hintText: "Name",
+                        prefixIcon: const Icon(Icons.person_outline),
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 18,
+                          horizontal: 20,
+                        ),
+                        filled: true,
+                        fillColor: Color(0xFFF7F7F9),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(14),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 24),
+                    TextField(
+                      controller: controller.emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        hintText: "Email address",
+                        prefixIcon: const Icon(Icons.email_outlined),
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 18,
+                          horizontal: 20,
+                        ),
+                        filled: true,
+                        fillColor: Color(0xFFF7F7F9),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(14),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 24),
+                    Obx(
+                      () => TextField(
+                        controller: controller.passwordController,
+                        obscureText: !controller.isPasswordVisible.value,
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                          prefixIcon: const Icon(Icons.lock_outline),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              controller.isPasswordVisible.value
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
+                            onPressed: controller.togglePasswordVisibility,
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: 18,
+                            horizontal: 20,
+                          ),
+                          filled: true,
+                          fillColor: Color(0xFFF7F7F9),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        "Password must be at least 8 characters",
+                        style: TextStyle(
+                          color: AppColors.subTextColor,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 63),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 48,
+                      child: ElevatedButton(
+                        onPressed: controller.isLoading.value
+                            ? null
+                            : controller.signIn,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.splashButtonColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(14),
+                          ),
+                        ),
+                        child: controller.isLoading.value
+                            ? const CircularProgressIndicator(
+                                color: Colors.white,
+                              )
+                            : const Text(
+                                "Sign Up",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white,
+                                ),
+                              ),
+                      ),
+                    ),
+                    SizedBox(height: 48),
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        text: "Don't have an account? ",
+                        style: const TextStyle(
+                          color: AppColors.subTextColor,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "  Sign In",
+                            style: const TextStyle(
+                              color: AppColors.splashButtonColor,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Get.back();
+                              },
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "Or",
+                      style: TextStyle(
+                        color: AppColors.subTextColor,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+
+                    SocialButton(
+                      imagePath: Iconpath.google,
+                      text: 'Continue with Google',
+                      onTap: () {},
+                    ),
+                    SizedBox(height: 24),
+                    SocialButton(
+                      imagePath: Iconpath.apple,
+                      text: 'Continue with Apple',
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -1,6 +1,7 @@
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:get/route_manager.dart';
 import 'package:yousef1234321/features/auth/sign_in/screen/sign_in_screen.dart';
+import 'package:yousef1234321/features/auth/sign_up/screen/sign_up_screen.dart';
 import 'package:yousef1234321/features/onboarding/screen/onboarding_screen.dart';
 import 'package:yousef1234321/features/splash/screen/splash_screen.dart';
 
@@ -8,14 +9,17 @@ class Approute {
   static String splashScreen = '/splashScreen';
   static String onboardingScreen = '/onboardingScreen';
   static String signInScreen = '/signInScreen';
+  static String signUpScreen = '/signUpScreen';
 
   static String getSplashScreen() => splashScreen;
   static String getOnboardingScreen() => onboardingScreen;
   static String getSignInScreen() => signInScreen;
+  static String getSignUpScreen() => signUpScreen;
 
   static List<GetPage> routes = [
     GetPage(name: splashScreen, page: () => SplashScreen()),
     GetPage(name: onboardingScreen, page: () => OnboardingScreen()),
     GetPage(name: signInScreen, page: () => SignInScreen()),
+    GetPage(name: signUpScreen, page: () => SignUpScreen()),
   ];
 }
