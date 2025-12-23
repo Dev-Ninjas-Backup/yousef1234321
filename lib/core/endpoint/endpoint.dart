4eb92beb29f1f77b390027e513424a4138d2cc94
tahmidulislamfahim <tahmidulislamfahim@gmail.com>
2025-12-24 05:33:52 +0600

    get profile data in profile page

--- lib/core/endpoint/endpoint.dart
+++ lib/core/endpoint/endpoint.dart
@@ -14,4 +14,5 @@ class Endpoint {
   static const String register = '/auth/register';
   static const String otpVerification = '/auth/signup-verify-otp';
   static const String login = '/auth/login';
+  static const String profile = '/user/me/profile';
 }
