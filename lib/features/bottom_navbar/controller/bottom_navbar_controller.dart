7f17e2eb72f96ff65aba3aa106e55d55f3b4abb8
tahmidulislamfahim <tahmidulislamfahim@gmail.com>
2026-07-15 18:02:39 +0600

    feat: implement nearby mode filtering and reset service state upon navigation

--- lib/features/bottom_navbar/controller/bottom_navbar_controller.dart
+++ lib/features/bottom_navbar/controller/bottom_navbar_controller.dart
@@ -1,9 +1,17 @@
 import 'package:get/get.dart';
+import '../../service/service page/controller/service_controller.dart';
 
 class BottomNavbarController extends GetxController {
   var currentIndex = 2.obs;
 
   void changeIndex(int index) {
     currentIndex.value = index;
+    if (index == 0) {
+      if (Get.isRegistered<ServiceController>()) {
+        final serviceController = Get.find<ServiceController>();
+        serviceController.radiusController.clear();
+        serviceController.resetToApprovedGarages();
+      }
+    }
   }
 }
